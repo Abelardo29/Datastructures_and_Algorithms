aa33c2f5f264f8233a106880011a3a49de7d8324
JANPC\Jan <jan.van.popta@hva.nl>
2017-09-10 01:00:13 +0200

    heb opdracht 1 + 2 opgeschoond/verbeterd, heb ook meteen tijdsberekening toegevoegd.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,47 +7,14 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
-            Random r = new Random();
-            int[] highscore = new int[100];
-            int max = 5000;
-            bool uniek = true;
-            int dub = 0;
+            Highscores highscore = new Highscores(5000);
+            highscore.Run();
 
-            for (int k = 0; k < max; k++)
-            {
-                for (int i = 0; i < highscore.Length; i++)
-                {
-                    highscore[i] = r.Next(0, 10000);
-                }
 
-                for (int i = 0; i < highscore.Length; i++)
-                {
-                    Console.WriteLine("This is spot = " + (i + 1) + " met nummer " + highscore[i]);
-                }
 
-                for (int i = 0; i < highscore.Length; i++)
-                {
-                    for (int j = i + 1; j < highscore.Length; j++)
-                    {
-                        if (i != j && highscore[i] == highscore[j] && highscore[i] != 0)
-                        {
-                            Console.WriteLine("Same score! " + highscore[i] + " spot " + (i + 1) + " spot " + (j + 1));
-                            highscore[i] = 0;
-                            highscore[j] = 0;
-                            uniek = false;
-                        }
-                    }
-                }
-                if (uniek == false)
-                {
-                    dub++;
-                }
-                uniek = true;
-            }
-            Console.WriteLine(dub);
             Console.ReadLine();
         }
     }
