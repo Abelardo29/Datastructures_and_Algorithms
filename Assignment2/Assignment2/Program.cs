2766e1edda34bfb9cb81861a1ae1fb712dac4a06
J.A. van Popta <jan.van.popta@hva.nl>
2017-09-18 11:57:31 +0200

    Eerste implementatie van de stacking assignment

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -7,6 +7,34 @@ using System.Threading.Tasks;
 namespace Assignment2 {
     class Program {
         static void Main(string[] args) {
+            List<string> listings = new List<string>();
+            bool running = true;
+
+            Console.WriteLine("Welcome to our stacking program! Write a line to add it to the card stackers. Write nothing to see the contents!");
+            //while (true) { var input = Console.ReadLine(); if (input == null || input == "") break; listings.Add(input); }
+
+            while (running) {
+
+                string userInput = Console.ReadLine();
+
+                if (userInput == "" || userInput == " ") {
+
+                    CustomStack printStack = new CustomStack(listings.Count);
+                    foreach (string current in listings) {
+                        printStack.Push(current);
+                    }
+                    while (!printStack.IsEmpty()) {
+                        printStack.Pop();
+                    }
+
+                    running = false;
+                } else {
+
+                    listings.Add(userInput);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Assignment2/Assignment2/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2 {
+    class CustomStack {
+        public int top = -1, maxSize;
+        protected string[] stackName;
+
+        public CustomStack(int maxSize) {
+            this.maxSize = maxSize;
+            stackName = new string[maxSize];            
+        }
+
+        public void Push(string content) {
+            if (!IsFull()) {
+                top++;
+                stackName[top] = content;
+            }
+        }
+
+        public string Pop() {
+            return stackName[top--];
+        }
+
+        public string Peek() {
+            if (!IsEmpty())
+                return stackName[top];
+            else return "no cards left!";
+        }
+
+        public bool IsEmpty() {
+            return top == -1;
+        }
+
+        public int Size() {
+            return stackName.Length;
+        }
+
+        public bool IsFull() {
+            return top == maxSize - 1;
+        }
+    }   
+}
