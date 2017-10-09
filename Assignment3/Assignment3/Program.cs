5fe6dacd7408d73e25efe88e1b027c76f67d1462
Jan Laptop <jan.van.popta@hva.nl>
2017-10-09 11:29:54 +0200

    Finishing first part of the finished state machine!

--- Assignment3/Assignment3/Program.cs
+++ Assignment3/Assignment3/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace Assignment3 {
     class Program {
         static void Main(string[] args) {
+            StateManager robbingState = new StateManager();
+            robbingState.GetNextScreen(robbingState.start);
+                Console.ReadLine();
         }
     }
 }
--- Assignment3/Assignment3/StateManager.cs
+++ Assignment3/Assignment3/StateManager.cs
@@ -14,70 +14,101 @@ namespace Assignment3 {
             LAYINGLOW
         }
 
-        private string
+        private const string
             whileRobbing = "I'm robbing banks and getting loads of money! Pew pew!",
             whileHavingGoodTime = "I'm having a good time spending my money",
             whileFleeing = "RUN FOREST RUN",
             whileLayingLow = "No one will find me now!",
-            getRich = "I'm rich enough to have a good time.",
-            spotCop = "I see a cop, so I have to start running",
-            feelSafe = "Fuck da police! I'm safe!",
-            getTired = "I'm getting very tired, so I better lay low for a while";
+            getRich = "get rich",
+            spotCop = "spot cop",
+            feelSafe = "feel safe",
+            GetTired = "get tired",
+            getRichText = "I'm rich enough to have a good time.",
+            spotCopText = "I see a cop, so I have to start running",
+            feelSafeText = "Fuck da police! I'm safe!",
+            getTiredText = "I'm getting very tired, so I better lay low for a while";
+
+        string robbingBank, havingGoodTime, fleeing, layingLow;
 
         public StateManager() {
-            string
-            robbingBank = getRich + "/" + spotCop + "," +  +,
-            havingGoodTime =,
-            fleeing =,
-            layingLow = ;
+            robbingBank = getRich + "/" + spotCop + "," + getRichText + "/" + spotCopText;
+            havingGoodTime = spotCop + "/" + GetTired + "," + spotCopText + "/" + getTiredText;
+            fleeing = feelSafe + "/" + GetTired + "/" + GetTired + "+" + getRich + "," + feelSafeText + "/" + getTiredText + "/" + getTiredText + " and " + getRichText;
+            layingLow = spotCop + "/" + feelSafe + "," + spotCopText + "/" + feelSafeText;
         }
-        
-        
-        
+
+
+
 
         List<string> optionList = new List<string>();
         List<string> responseList = new List<string>();
 
-        private STATE start = STATE.ROBBING_BANK;
+        public STATE start = STATE.ROBBING_BANK;
         private int moneyCurrent = 0;
 
         public void GetNextScreen(STATE currentState) {
             switch (currentState) {
                 case STATE.ROBBING_BANK:
-                    Console.WriteLine("I'm robbing banks and getting loads of money! Pew pew!");
-                    RobBank();
+                    Console.WriteLine(whileRobbing);
+                    GetNextScreen(Situation(robbingBank));
                     break;
 
                 case STATE.HAVING_GOOD_TIME:
-                    Console.WriteLine("I'm having a good time spending my money");
-                    HaveGoodTime();
+                    Console.WriteLine(whileHavingGoodTime);
+                    GetNextScreen(Situation(havingGoodTime));
                     break;
 
                 case STATE.FLEEING:
+                    Console.WriteLine(whileFleeing);
+                    GetNextScreen(Situation(fleeing));
                     break;
 
                 case STATE.LAYINGLOW:
+                    Console.WriteLine(whileLayingLow);
+                    GetNextScreen(Situation(layingLow));
                     break;
             }
         }
 
-        private void RobBank() {
-            Console.WriteLine("What should i do next?");
-            Console.WriteLine("(get rich or spot cop)");
-            switch (Console.ReadLine()) {//hier vragen we de speler input aan.
-                case "get rich"://cases zoals gewoonlijk.
-                    Console.WriteLine("I'm rich enough to have a good time");
-                    GetNextScreen(STATE.HAVING_GOOD_TIME);
-                    break;
-                case "spot cop":
-                    Console.WriteLine("I see a cop, so I have to start running");
-                    GetNextScreen(STATE.FLEEING);
-                    break;
+        private STATE Situation(string input) {
+            string[] seperate = input.Split(',');
+            string[] options = seperate[0].Split('/');
+            string[] dialogue = seperate[1].Split('/');
+
+            Console.WriteLine("What should i do? ");
+            Console.Write("I can do nothing");
+
+            for (int i = 0; i < options.Length; i++) {
+                Console.Write(" or " + options[i]);
             }
+
+            Console.WriteLine();
+
+            while (true) {
+                string userInput = Console.ReadLine();
+                for (int i = 0; i < options.Length; i++) {
+                    if (userInput.Equals(options[i])) {
+                        Console.WriteLine(dialogue[i]);
+                        return GetEnum(options[i]);
+                    }
+                }
+            }
+
         }
 
-        private void HaveGoodTime() {
-
+        private STATE GetEnum (string stringToEnum) {
+            switch (stringToEnum) {
+                case getRich:
+                    return STATE.HAVING_GOOD_TIME;
+                case spotCop:
+                    return STATE.LAYINGLOW;
+                case feelSafe:
+                    return STATE.ROBBING_BANK;
+                case GetTired:
+                    return STATE.LAYINGLOW;
+                default:
+                    return start;
+            }
         }
 
     }
